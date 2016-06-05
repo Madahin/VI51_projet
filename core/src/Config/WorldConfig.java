a544fe0cc870b18c4ed62b509f8c2e58887a5762
Tiras69 <mathm69@gmail.com>
2016-06-05 12:46:37 +0200

    shitty commit for merging une fracking file

--- core/src/Config/WorldConfig.java
+++ core/src/Config/WorldConfig.java
@@ -14,11 +14,11 @@ public class WorldConfig {
 	
 	public static int MIN_SIZE_FOOD_STACK = 3;
 
-	public static int PHEROMONE_INITIAL_LIFE = 25;
+	public static int PHEROMONE_INITIAL_LIFE = 100;
 	
 	public static int ANT_FIELD_OF_VIEW = 5;
 	
 	public static boolean SMOOTH_FOOD_GENERATION = true;
 	
-	public static float FOOD_COVER_PERCENT = 1f;
+	public static float FOOD_COVER_PERCENT = 0.5f;
 }
